6ac8ac05a15a80ac9578b902695db37f9130fb11
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-10 12:04:14 +0200

    aggiunto FK in posts

---
+++ database/migrations/2022_07_10_094021_update_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            /* DropForeign si aspetta un array, in caso ci sono più FK, quindi vanno messe le quadre*/
+            $table->dropForeign(['category_id']); /* Droppo il contenuto */
+            $table->dropColumn(['category_id']); /* Droppo la colonna */
+        });
+    }
+}
