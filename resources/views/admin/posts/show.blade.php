a5941066f35f626483e8b25631c9a3f31c1aa8ad
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-10 13:25:36 +0200

    aggiunta category nello show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,6 +5,7 @@
 <h1>Author Post Details</h1>
 
 <h3>Name: {{ $post->name }}</h3>
+<h3>Category: {{ $post->category->name }}</h3>
 <h3>Location: {{ $post->location }}</h3>
 <h3>Email: {{ $post->email }}</h3>
 <h3>ID Post: {{ $post->id }}</h3>
